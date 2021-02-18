662556853920c1adbdf0b31b77c23c0203c30f85
Crowdbotics <team@crowdbotics.com>
2021-02-18 21:11:10 +0000

    Update models

--- backend/spunky_spice_magazi_24602/wsgi.py
+++ backend/spunky_spice_magazi_24602/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spunky_spice_magazi_24602.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spunky_spice_magazi_24602.settings")
 
 application = get_wsgi_application()
